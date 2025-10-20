69360ed2b35f3e6b131528d5334f4461c3d2cf63
Bombus10 <xblythe@harmonyschool.org>
2025-10-20 14:15:36 -0400

    UpdateScriptii

--- update_script.sh
+++ update_script.sh
@@ -7,7 +7,7 @@ sudo apt -y update && sudo apt -y upgrade
 echo 'updating snap packages'
 sudo snap refresh
 
-if ! snap list | grep -q 'opera'; then
+if ! snap list | grep -q '^opera'; then
     echo 'installing opera'
     sudo snap install opera
 else
@@ -48,7 +48,7 @@ else
     echo 'less already configured to use source highlight, skipping'
 fi
 
-if ! find 'custom-hot-#!/bin/bash
+if ! find 'custom-hot-#!/bin/bash'
 
 
 echo 'updating your system'
@@ -134,53 +134,4 @@ echo 'cleaning up'
 sudo apt -y autoremove
 
 echo 'hot corners extension needs to be configured. Click the circle on the far right of your task bar (show it by hovering your cursor over the bottom of the screen.) Click Extension manager, click the gear next to Custom Hot Corners - Extended. You can customize from there what certain buttons do.'
-echo "thank you for using Xander's update drive"corners*'; then
-    echo 'installing custom hot corners extension'
-    curl -L -o custom-hot-corners-extended@G-dH.github.com.zip https://github.com/G-dH/custom-hot-corners-extended/releases/latest/download/custom-hot-corners-extended@G-dH.github.com.zip && mv custom-hot-corners-extended@G-dH.github.com.zip /tmp/
-    gnome-extensions install --force /tmp/custom-hot-corners-extended@G-dH.github.com.zip
-    gnome-extensions enable custom-hot-corners-extended@G-dH.github.com
-else
-    echo 'custom hot corners extension already installed, skipping'
-fi
-
-if ! find /etc/apt/sources.list.d/ -name 'vscode.sources'; then
-    echo 'installing visual studio code'
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/keyrings/packages.microsoft.gpg 
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-    sudo apt update && sudo apt install code
-else 
-    echo 'visual studio code already installed, skipping'
-fi
-
-if ! gsettings get org.gnome.shell favorite-apps | grep -q 'opera_opera'; then
-    echo 'setting up system preferences'
-    sudo apt -y install gnome-shell-extension-manager
-    gsettings set org.gnome.shell favorite-apps "['opera_opera.', 'code_code.desktop', 'org.gnome.Nautilus.desktop', 'gnome-terminal.desktop',]"
-    gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-    gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
-    gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-    gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
-else
-    echo 'system preferences already set, skipping'
-fi
-
-githubusername=$(git config user.name)
-
-read -p "Is your github username: $githubusername ? (y/n)" answer
-if [ "$answer" != "${answer#[Yy]}" ] ;then
-    echo 'Configuring github'
-    read -p "Enter Github Username: " githubusername
-    echo 'put in your Github email'
-    read -p "enter Github Email:" githubemail
-    git config --global user.name "$githubusername"
-    git config --global user.email "$githubemail"
-else
-    echo "Github already configured, skipping"
-fi
-
-
-echo 'cleaning up'
-sudo apt -y autoremove
-
-echo 'hot corners extension needs to be configured. Click the circle on the far right of your task bar (show it by hovering your cursor over the bottom of the screen.) Click Extension manager, click the gear next to Custom Hot Corners - Extended. You can customize from there what certain buttons do.'
-echo "thank you for using Xander's update drive"
+echo "thank you for using Xander's update drive"corners*'; 
