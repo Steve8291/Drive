b27155476e91620cc8dc3c72fa4f21fda552513c
Bombus10 <xblythe@harmonyschool.org>
2025-10-21 09:47:59 -0400

    Problem with viarables

--- update_script.sh
+++ update_script.sh
@@ -48,48 +48,7 @@ else
     source ~/.bashrc
 fi
 
-# if ! find 'custom-hot-#!/bin/bash'
-
-
-# echo 'updating your system'
-# sudo apt -y update && sudo apt -y upgrade
-
-# echo 'updating snap packages'
-# sudo snap refresh
-
-# if ! snap list | grep -q 'opera'; then
-#     echo 'installing opera'
-#     sudo snap install opera
-# else
-#     echo 'opera already installed, skipping'
-# fi
-
-# echo 'enabling firewall'
-# ufw enable
-
-# if ! dpkg -l | grep -q 'plocate'; then
-#     echo 'installing useful packages'
-#     sudo apt -y install plocate curl
-# else 
-#     echo 'plocate already installed, skipping'
-# fi
-
-# if ! dpkg -l | grep -q 'source-highlight'; then
-#     echo 'enabling source highlight for less'
-#     sudo apt -y install source-highlight
-# else 
-#     echo 'source-highlight already installed, skipping'
-# fi
-
-# if ! grep -q 'source-highlight' ~/bashrc; then
-#     echo 'configuring less to use source highlight'
-#     echo 'export LESSOPEN="| /usr/share/source-highlight/src-hilite-lesspipe.sh %s' >> ~/.bashrc
-#     echo "export LESS=' -cR '" >> ~/.bashrc
-# else
-#     echo 'less already configured to use source highlight, skipping'
-# fi
-
-# if ! find 'custom-hot-corners*'; then
+# if [ !  -d "~/.local/share/gnome-shell/extensions/custom-hot-corners-extended@G-dH.github.com" ]; then
 #     echo 'installing custom hot corners extension'
 #     curl -L -o custom-hot-corners-extended@G-dH.github.com.zip https://github.com/G-dH/custom-hot-corners-extended/releases/latest/download/custom-hot-corners-extended@G-dH.github.com.zip && mv custom-hot-corners-extended@G-dH.github.com.zip /tmp/
 #     gnome-extensions install --force /tmp/custom-hot-corners-extended@G-dH.github.com.zip
@@ -98,19 +57,19 @@ fi
 #     echo 'custom hot corners extension already installed, skipping'
 # fi
 
-# if ! find /etc/apt/sources.list.d/ -name 'vscode.sources'; then
-#     echo 'installing visual studio code'
-#     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/keyrings/packages.microsoft.gpg 
-#     echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-#     sudo apt update && sudo apt install code
-# else 
-#     echo 'visual studio code already installed, skipping'
-# fi
+if ! find /etc/apt/sources.list.d/ -name 'vscode.sources'; then
+    echo 'installing visual studio code'
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/keyrings/packages.microsoft.gpg 
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
+    sudo apt update && sudo apt install code
+else 
+    echo 'visual studio code already installed, skipping'
+fi
 
 # if ! gsettings get org.gnome.shell favorite-apps | grep -q 'opera_opera'; then
 #     echo 'setting up system preferences'
 #     sudo apt -y install gnome-shell-extension-manager
-#     gsettings set org.gnome.shell favorite-apps "['opera_opera.', 'code_code.desktop', 'org.gnome.Nautilus.desktop', 'gnome-terminal.desktop',]"
+#     gsettings set org.gnome.shell favorite-apps "['opera_opera.desktop', 'org.gnome.Terminal.desktop', 'code.desktop', 'org.gnome.Nautilus.desktop']"
 #     gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 #     gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
 #     gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
@@ -119,25 +78,17 @@ fi
 #     echo 'system preferences already set, skipping'
 # fi
 
-# githubusername=$(git config user.name)
-
-# echo 'cleaning up'
-# sudo apt -y autoremove
-
-# echo 'hot corners extension needs to be configured. Click the circle on the far right of your task bar (show it by hovering your cursor over the bottom of the screen.) Click Extension manager, click the gear next to Custom Hot Corners - Extended. You can customize from there what certain buttons do.'
-# echo "thank you for using Xander's update drive"
-
-# read -p "Is your github username: $githubusername ? (y/n)" answer
-# if [ "$answer" != "${answer#[Yy]}" ] ;then
-#     echo 'Configuring github'
-#     read -p "Enter Github Username: " githubusername
-#     echo 'put in your Github email'
-#     read -p "enter Github Email:" githubemail
-#     git config --global user.name "$githubusername"
-#     git config --global user.email "$githubemail"
-# else
-#     echo "Github already configured, skipping"
-# fi
+echo $githubusername
+read -p 'Is your github username? (y/n)' answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+    echo "Github already configured, skipping"
+else
+    echo 'Configuring github'
+    read -p "Enter Github Username: " githubusername
+    read -p "Enter Github Email: " githubemail
+    # git config --global user.name "$githubusername"
+    # git config --global user.email "$githubemail"
+fi
 
 
 # echo 'cleaning up'
